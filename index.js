2d184ac5bc602531784b72c44e810fd79fe002fc
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-11 17:02:22 +0200

    change root and catch parse content

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 const user = process.argv[2] || 'de7ecd1e2976a6adb2ffa5f4db81a7d812c8bb6698aa00dcf1e76adb55efd645'
 
-const root = './cache'
+const root = './cache/.well-known/nostr/pubkey'
 const cacheDirectory = path.join(root, `${user}`)
 const filname = 'index.json'
 const cacheFilePath = path.join(cacheDirectory, filname)
@@ -16,37 +16,43 @@ fs.mkdirSync(cacheDirectory, { recursive: true })
 // Create an object to store the merged data
 let mergedData = {}
 
-function parseEvent (event) {
-  if (event.kind === 0) {
-    const json = JSON.parse(event.content)
-    return json
-  } else if (event.kind === 3) {
-    const tags = event.tags
-    const contacts = []
-    tags.forEach(element => {
-      if (element[0] === 'p') {
-        contacts.push('nostr:pubkey:' + element[1])
-      }
-    })
+function parseEvent(event) {
+	if (event.kind === 0) {
+		const json = JSON.parse(event.content)
+		return json
+	} else if (event.kind === 3) {
+		const tags = event.tags
+		const contacts = []
+		tags.forEach(element => {
+			if (element[0] === 'p') {
+				contacts.push('nostr:pubkey:' + element[1])
+			}
+		})
 
-    function transformRelays (relays) {
-      return Object.entries(relays).map(([key, value]) => {
-        const modes = []
-        if (value.read) modes.push('read')
-        if (value.write) modes.push('write')
-        return {
-          '@id': key,
-          mode: modes
-        }
-      })
-    }
-    let relays = JSON.parse(event.content)
-    relays = transformRelays(relays)
-    // console.log(relays)
+		function transformRelays(relays) {
+			return Object.entries(relays).map(([key, value]) => {
+				const modes = []
+				if (value.read) modes.push('read')
+				if (value.write) modes.push('write')
+				return {
+					'@id': key,
+					mode: modes
+				}
+			})
+		}
+		try {
+			let relays = JSON.parse(event.content)
+			relays = transformRelays(relays)
+		} catch (e) {
+		}
+		// console.log(relays)
 
-    const json = { following: contacts, relay: relays }
-    return json
-  }
+		if (relays) {
+			return { following: contacts, relay: relays }
+		} else {
+			return { following: contacts }
+		}
+	}
 }
 
 const scsi = 'wss://nostr-pub.wellorder.net'
@@ -54,23 +60,23 @@ const relay = [scsi]
 
 const pool = RelayPool(relay)
 pool.on('open', relay => {
-  relay.subscribe('subid', { limit: 5, kinds: [0, 3], authors: [user] })
+	relay.subscribe('subid', { limit: 5, kinds: [0, 3], authors: [user] })
 })
 
 pool.on('event', (relay, sub_id, ev) => {
-  const parsedEvent = parseEvent(ev)
-  // Merge the parsed event into the mergedData object
-  mergedData = { ...parsedEvent, ...mergedData }
+	const parsedEvent = parseEvent(ev)
+	// Merge the parsed event into the mergedData object
+	mergedData = { ...parsedEvent, ...mergedData }
 
-  console.log(mergedData)
+	console.log(mergedData)
 
-  fs.writeFileSync(cacheFilePath, JSON.stringify({
-    '@id': '',
-    mainEntity: {
-      '@id': 'nostr:pubkey:' + user,
-      ...mergedData
-    }
-  }, null, 2))
+	fs.writeFileSync(cacheFilePath, JSON.stringify({
+		'@id': '',
+		mainEntity: {
+			'@id': 'nostr:pubkey:' + user,
+			...mergedData
+		}
+	}, null, 2))
 
-  relay.close()
+	relay.close()
 })
